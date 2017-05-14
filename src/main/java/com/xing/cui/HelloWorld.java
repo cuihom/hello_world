cdf31f758cc1d0d95b00396611d566036ffb3bd9
cuihom <cuihom@cuihomdeMacBook-Pro.local>
2017-05-14 11:35:44 +0800

    111

--- src/main/java/com/xing/cui/HelloWorld.java
+++ src/main/java/com/xing/cui/HelloWorld.java
@@ -3,12 +3,9 @@ package com.xing.cui;
 public class HelloWorld {
 	
 	public static void main(String[]args){
-<<<<<<< Updated upstream
       System.out.print("hello world 1123");
 
-=======
-		System.out.print("hello world 1123");
->>>>>>> Stashed changes
+
 	}
 
 }
