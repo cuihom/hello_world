5d85f32ce7956591c444d140ba143d3bcd64f6fd
cuihom <cuihom@cuihomdeMacBook-Pro.local>
2017-05-14 19:47:19 +0800

    hello 2

--- src/main/java/com/xing/cui/HelloWorld.java
+++ src/main/java/com/xing/cui/HelloWorld.java
@@ -3,7 +3,7 @@ package com.xing.cui;
 public class HelloWorld {
 	
 	public static void main(String[]args){
-      System.out.print("hello world 1123");
+      System.out.print("hello world 112311");
 
 
 	}
